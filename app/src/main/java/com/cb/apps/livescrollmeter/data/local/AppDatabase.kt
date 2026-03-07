d40936adbd3f74ae2a34721c46d4cf10d64a618c
Amrish Sharma <amrishsharma95@gmail.com>
2026-03-07 19:18:53 +0530

    added github action

--- app/src/main/java/com/cb/apps/livescrollmeter/data/local/AppDatabase.kt
+++ app/src/main/java/com/cb/apps/livescrollmeter/data/local/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.cb.apps.livescrollmeter.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [SessionEntity::class], version = 1)
+@Database(entities = [SessionEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun sessionDao(): SessionDao
 }
