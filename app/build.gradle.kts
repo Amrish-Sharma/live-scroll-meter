5b8cc67b396f056940e9bb4f8e9d1ded7fd5f965
Amrish Sharma <amrishsharma95@gmail.com>
2026-03-07 20:45:28 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(keystoreProperties["storeFile"] as String)
+            storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
             storePassword = keystoreProperties["storePassword"] as String
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
